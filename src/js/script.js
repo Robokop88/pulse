af403417c6d8b2d72fa7c9eaa151fab233635856
dima <salaev334@gmail.com>
2022-08-30 23:28:42 +0300

    tabs_animated

--- src/js/script.js
+++ src/js/script.js
@@ -27,8 +27,14 @@ $(document).ready(function(){
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active animate__animated animate__fadeInUp wow');
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('animate__ animate__fadeInUp wow')
+  });
+
 
 
   function toggleSlide(item) {
@@ -43,6 +49,8 @@ $(document).ready(function(){
   
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+
 
   // Modal
    
