c70292ee59bf3fb125732ed73a5b92fb9ba1b443
dima <salaev334@gmail.com>
2022-09-06 19:33:44 +0300

    Promo_position

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'ssl://smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'Ddss881@yandex.ru';                 // Наш логин
-$mail->Password = 'rmmdjzmemwbyytkx';                           // Наш пароль от ящика
+$mail->Password = 'Kalibr2!';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
